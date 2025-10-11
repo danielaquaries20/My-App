436dc9b0543d900fba6bd450c24d601ddfc345a9
danielaquaries20 <danielaquaries@gmail.com>
2025-10-11 09:24:12 +0700

    Update Counter App - KI Ch.2

--- app/src/main/java/com/daniel/myapp/middle/mvvm_counter/MvvmCounterActivity.kt
+++ app/src/main/java/com/daniel/myapp/middle/mvvm_counter/MvvmCounterActivity.kt
@@ -1,36 +1,40 @@
 package com.daniel.myapp.middle.mvvm_counter
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.crocodic.core.base.activity.CoreActivity
+import com.crocodic.core.extension.openActivity
 import com.daniel.myapp.R
 import com.daniel.myapp.databinding.ActivityMvvmCounterBinding
+import com.daniel.myapp.middle.aa_shared_pref.MiddleSession
+import com.daniel.myapp.middle.counter.CounterActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MvvmCounterActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: MvvmCounterViewModel
-    private lateinit var binding: ActivityMvvmCounterBinding
-
+@AndroidEntryPoint
+class MvvmCounterActivity : CoreActivity<ActivityMvvmCounterBinding, MvvmCounterViewModel>(R.layout.activity_mvvm_counter) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 //        setContentView(R.layout.activity_mvvm_counter)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm_counter)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm_counter)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        viewModel = ViewModelProvider(this)[MvvmCounterViewModel::class.java]
+//        viewModel = ViewModelProvider(this)[MvvmCounterViewModel::class.java]
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
-
     }
+
 }
--- app/src/main/java/com/daniel/myapp/middle/mvvm_counter/MvvmCounterViewModel.kt
+++ app/src/main/java/com/daniel/myapp/middle/mvvm_counter/MvvmCounterViewModel.kt
@@ -1,19 +1,39 @@
 package com.daniel.myapp.middle.mvvm_counter
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.crocodic.core.base.viewmodel.CoreViewModel
+import com.daniel.myapp.middle.aa_shared_pref.MiddleSession
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
-class MvvmCounterViewModel : ViewModel() {
 
-    private val _count = MutableStateFlow(0)
+@HiltViewModel
+class MvvmCounterViewModel @Inject constructor(private val middleSession: MiddleSession) : CoreViewModel() {
+
+    private val _count = MutableStateFlow(middleSession.getCount())
     val count: StateFlow<Int> = _count
 
     fun increment() {
         _count.value += 1
+        middleSession.saveCount(_count.value)
     }
 
     fun decrement() {
         _count.value -= 1
+        middleSession.saveCount(_count.value)
     }
+
+    override fun apiLogout() {}
+
+    override fun apiRenewToken() {}
 }
+
+/*
+class Factory(private val middleSession: MiddleSession): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MvvmCounterViewModel(middleSession) as T
+    }
+}*/
