25641abddad166657c4cc5f3fffea01352dacecc
danielaquaries20 <danielaquaries@gmail.com>
2026-01-15 13:40:36 +0700

    Update Friend App - Trial Ch 15 P1 - Complete.

---
+++ app/src/main/java/com/daniel/myapp/notification/BasicNotificationActivity.kt
@@ -0,0 +1,134 @@
+package com.daniel.myapp.notification
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.crocodic.core.base.activity.CoreActivity
+import com.crocodic.core.base.activity.NoViewModelActivity
+import com.crocodic.core.extension.snacked
+import com.daniel.myapp.R
+import com.daniel.myapp.app_tour.ui.home.HomeViewModel
+import com.daniel.myapp.databinding.ActivityBasicNotificationBinding
+import com.daniel.myapp.maps.MapsActivity
+import com.daniel.myapp.the_twin_binding.HomeActivity
+
+class BasicNotificationActivity :
+    CoreActivity<ActivityBasicNotificationBinding, HomeViewModel>(R.layout.activity_basic_notification) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkNotificationPermission()
+        }
+
+        binding.btnShowNotif.setOnClickListener {
+            showNotification("Hallo Gais!", "Saya sedang belajar membuat notifikasi.")
+        }
+
+    }
+
+    private fun showNotification(title: String, content: String) {
+
+        createNotificationChannel()
+
+        val intent = Intent(this, MapsActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_circle_notification)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    this@BasicNotificationActivity,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            notify(1, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = CHANNEL_DESCRIPTION
+            }
+
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkNotificationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {}
+
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                binding.root.snacked(
+                    message = R.string.lbl_permission_notification,
+                    action = R.string.lbl_setting,
+                    listener = {
+                        val intent = Intent()
+                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                        intent.data = Uri.fromParts("package", packageName, null)
+                        startActivity(intent)
+                    }
+                )
+            }
+
+            else -> {
+                // You can directly ask for the permission.
+                requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), 100)
+            }
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "Kelas Industri"
+        const val CHANNEL_NAME = "Kelas Industri Android"
+        const val CHANNEL_DESCRIPTION = "Kelas Industri Android Level Middle"
+    }
+}
